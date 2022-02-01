58afcd1468a8842ad02b822f16f47c4431f06e38
EugenePoloshovets <jeksoncheggg@gmail.com>
2022-02-01 21:28:14 +0300

    string && stringBuilder

---
+++ src/payau/core/string/Task1.java
@@ -0,0 +1,12 @@
+package payau.core.string;
+// Замнить все грустные смайлы :( на веселые ):
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        String string1 = "df34:(daffsda 21441 :)afsdf :( efad ";
+        String string2 = string1.replace(":(", ":)");
+        System.out.println(string2);
+
+    }
+}
---
+++ src/payau/core/string/Task3.java
@@ -0,0 +1,29 @@
+package payau.core.string;
+
+/**
+ * Написать функцию, принимающую в качетсве параметров имя,фамилию и отчество и возвращающую инициалы
+ * в формате Ф.И.О . Результат в верхнем регистре, а входные - в любом регистре.
+ */
+
+import java.util.Locale;
+
+public class Task3 {
+    public static void main(String[] args) {
+
+        String name = "Vas9";
+        String surname = "ivanov";
+        String patronymic = "LIvanovich";
+        System.out.println(format(name, surname, patronymic));
+
+    }
+
+    public static String format(String name, String surname, String patronymic) {
+
+        char nameChar = name.toUpperCase().charAt(0);
+        char surnameChar = surname.toUpperCase().charAt(0);
+        char patronymicChar = patronymic.toUpperCase().charAt(0);
+
+        return String.format("%s.%s.%s.", nameChar, surnameChar, patronymicChar);
+
+    }
+}
