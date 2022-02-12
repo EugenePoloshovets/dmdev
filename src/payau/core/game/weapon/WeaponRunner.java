6d279ff2c8ed562bcbf2324af21db0492df7ec96
EugenePoloshovets <jeksoncheggg@gmail.com>
2022-02-12 19:58:02 +0300

    generics in methods

--- src/payau/core/game/weapon/WeaponRunner.java
+++ src/payau/core/game/weapon/WeaponRunner.java
@@ -1,6 +1,7 @@
 package payau.core.game.weapon;
 
 import payau.core.game.Archer;
+import payau.core.game.Hero;
 import payau.core.game.Warrior;
 
 public class WeaponRunner {
@@ -13,5 +14,24 @@ public class WeaponRunner {
         Warrior<Sword> warrior = new Warrior<>("boromir", 10);
         warrior.setWeapon(new Sword());
 
+        printWeaponDamage(archer);
+        printWeaponDamage(warrior);
+
     }
+
+    public static <T extends Weapon> void printWeaponDamage(Hero<T> hero) {
+        System.out.println(hero.getWeapon().getDamage());
+    }
+
+/*
+    public static void printWeaponDamage(Hero<? extends Weapon> hero) {
+        // hero.setWeapon(new Sword()); -- ошибка тк producer
+        System.out.println(hero.getWeapon().getDamage());
+    }
+
+    public static void printWeaponDamage(Hero<? super Sword> hero) {
+        hero.setWeapon(new Sword()); -- consumer
+        System.out.println(hero.getWeapon().getDamage());
+    }
+*/
 }
